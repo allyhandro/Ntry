d3aa2fcebc94f17634e68096c36da36d4c55e52c
cl2852 <cl2852@nyu.edu>
2017-04-18 21:01:38 -0400

    Basic Layout w Sidebar

---
+++ 'Ntry/'Ntry/ArtTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ArtViewController.swift
+//  'Ntry
+//
+//  Created by Caroline Lai on 4/18/17.
+//  Copyright © 2017 Caroline Lai. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArtTableViewController: UITableViewController{
+    
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ 'Ntry/'Ntry/ScanViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ScanViewController.swift
+//  'Ntry
+//
+//  Created by Caroline Lai on 4/18/17.
+//  Copyright © 2017 Caroline Lai. All rights reserved.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController{
+    
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
