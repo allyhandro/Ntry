869b6101221c6971c12d340ccc31e95528f60970
allyhandro <awh264@gmail.com>
2017-05-01 01:13:21 -0400

    deploy try

--- NtryWebPortal/routes/api.js
+++ NtryWebPortal/routes/api.js
@@ -4,3 +4,7 @@ const mongoose = require('mongoose');
 require('../db');
 const Item = mongoose.model('Item');
 
+router.get('/', (req, res)=>{
+    res.render('index', {layout:'layout'});
+});
+module.exports = router;
