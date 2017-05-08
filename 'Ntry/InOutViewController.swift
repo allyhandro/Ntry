66e7e85095b9a705d7273eedea82eb00c342a3eb
cl2852 <cl2852@nyu.edu>
2017-05-08 00:49:16 -0400

    request changes

--- 'Ntry/InOutViewController.swift
+++ 'Ntry/InOutViewController.swift
@@ -39,18 +39,15 @@ class InOutViewController: UIViewController{
         if(artLabel.text != "" && artLabel.text != "No QR code is detected"){
     
             //I. GET the status using artLabel.text
-            let urlString = "https://ntry.herokuapp.com/api/items/" + artLabel.text! + "/_move"
+            let urlString = "https://ntry.herokuapp.com/api/items/" + artLabel.text! + "/_check_in_out"
             let url = NSURL(string: urlString)
             let request = NSMutableURLRequest(url: url! as URL)
             request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type") //Optional
-            request.httpMethod = "GET"
+            request.httpMethod = "PUT"
         
             let session = URLSession(configuration:URLSessionConfiguration.default, delegate: nil, delegateQueue: nil)
-        
-            //"GET" and store "status" from the database based on the "artLabel.text & changeStatus to opposite of "status"
-            //request to get the status of item
-            var status:String!
             
+            //make the request to change the status
             let dataTask = session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
                 
                 if error != nil {
@@ -66,94 +63,10 @@ class InOutViewController: UIViewController{
                     self.present(alert, animated: true, completion: nil)
                 }
                 else {
-                
-                    do{
-                        //retrieve the status
-                        let jsonStr = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:String]
-                        status = jsonStr?["status"] as? String!
-                        print ("The status of " + self.artLabel.text! + "is: " + status)
-
-                        print("Parsed JSON: '\(jsonStr)'")
-                        
-                        
-                        //II.Changing the status
-                        var changeStatus:String!
-                        if(status.isEqual("in")){
-                            changeStatus = "status=off"
-                        }
-                        if(status.isEqual("out")){
-                            changeStatus = "status=on"
-                        }else{
-                            let alerted = UIAlertController(title: "Uh-oh!", message: "Something went wrong", preferredStyle: UIAlertControllerStyle.alert)
-                            
-                            // add an action (button)
-                            alerted.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-                            
-                            // show the alert
-                            self.present(alerted, animated: true, completion: nil)
-                            
-                        }
-                        
-                        //Attempt to change the database
-                        let changeRequest = NSMutableURLRequest(url: url! as URL)
-                        changeRequest.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type") //Optional
-                        changeRequest.httpMethod = "PUT"
-                        let changeSession = URLSession(configuration:URLSessionConfiguration.default, delegate: nil, delegateQueue: nil)
-                        
-                        let changeData = changeStatus.data(using: String.Encoding.utf8)
-                        changeRequest.httpBody = changeData
-                        let changeDataTask = changeSession.dataTask(with: changeRequest as URLRequest) { (changeData, changeResponse, error) -> Void in
-                            if error != nil {
-                                print(error!)
-                                
-                                // create the alert
-                                let alerter = UIAlertController(title: "Uh-oh!", message: "Something went wrong", preferredStyle: UIAlertControllerStyle.alert)
-                                
-                                // add an action (button)
-                                alerter.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-                                
-                                // show the alert
-                                self.present(alerter, animated: true, completion: nil)
-                            }
-                            else {//pushing the change
-                                let jsonStr = NSString(data: changeData!, encoding: String.Encoding.utf8.rawValue)
-                                print("Parsed JSON: '\(jsonStr)'")
-                                
-                                // create the alert
-                                let alert = UIAlertController(title: "Success", message: "Update successful", preferredStyle: UIAlertControllerStyle.alert)
-                                
-                                // add an action (button)
-                                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-                                
-                                // show the alert
-                                self.present(alert, animated: true, completion: nil)
-                            }
-                        }
-                        changeDataTask.resume()
-                        
-                        
-                        // create the alert
-                        let alert = UIAlertController(title: "Success", message: "Update successful", preferredStyle: UIAlertControllerStyle.alert)
-                        
-                        // add an action (button)
-                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-                    
-                        // show the alert
-                        self.present(alert, animated: true, completion: nil)
-                        
-                        
-                        
-                        
-                    }catch{ //error if could not complete "request"
-                        let alertt = UIAlertController(title: "Uh-oh!", message: "Something went wrong", preferredStyle: UIAlertControllerStyle.alert)
-                        
-                        // add an action (button)
-                        alertt.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-                        
-                        // show the alert
-                        self.present(alertt, animated: true, completion: nil)
-
-                    }
+                    let httpResponse = response as? HTTPURLResponse
+                    let stringResponse = httpResponse?.mimeType?.lowercased()
+                    print(stringResponse)
+                    self.statusLabel.text = stringResponse
                 }
             }
             dataTask.resume()
