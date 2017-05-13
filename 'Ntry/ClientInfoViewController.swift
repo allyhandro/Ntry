8e04ba71af3e026344d620b8eb487c10fc7b6637
allyhandro <awh264@gmail.com>
2017-05-12 21:50:20 -0400

    added art stuff

--- 'Ntry/ClientInfoViewController.swift
+++ 'Ntry/ClientInfoViewController.swift
@@ -13,7 +13,7 @@ class ClientInfoTableViewController:UITableViewController{
 
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
-    
+    @IBOutlet var tbView: UITableView!
     @IBOutlet weak var navBar: UINavigationItem!
     var clientName = String()
     var clientId = String()
@@ -24,6 +24,8 @@ class ClientInfoTableViewController:UITableViewController{
     override func viewDidLoad() {
         self.navBar.title = clientName
         super.viewDidLoad()
+        tableView.dataSource = self;
+        tableView.delegate = self;
         if self.revealViewController() != nil {
             self.revealViewController().rearViewRevealWidth = 165
             menuButton.target = self.revealViewController()
@@ -35,10 +37,60 @@ class ClientInfoTableViewController:UITableViewController{
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    func fetchClientItems(){
+        let url = NSURL(string: "https://ntry.herokuapp.com/api/clients/" + clientId + "/_findItems")
+        URLSession.shared.dataTask(with: url! as URL){ (data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            do {
+                ArtInfo.pieces.removeAll()
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                for dictionary in json as! [[String: AnyObject]]{
+                    print(dictionary["title"]!)
+                    ArtInfo.artTitles.append(dictionary["title"] as! String)
+                }
+                ArtInfo.artTitles = ArtInfo.artTitles.sorted { $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
+                
+                
+                
+            } catch let jsonError {
+                print(jsonError)
+            }
+            
+            }.resume()
+    }
+    
+    
+    /*
+    override func viewWillAppear(_ animated: Bool) {
+        //self.fetchClients()
+        super.viewWillAppear(animated)
+        tbView.reloadData()
+    }
+ */
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ArtInfo.artTitles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"ArtCell",for:indexPath)
+        cell.textLabel?.text = ArtInfo.artTitles[indexPath.row]
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath
+        indexPath: NSIndexPath){
+        self.performSegue(withIdentifier: "showItemInfo", sender: self)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreat
     }
-    func fetchClientInfo(){
-    }
+
 }
